303d09a193e91579d397a3fbe27326db5098bfbb
Swelen Cesario Ebert <swelens2@hotmail.com>
2021-04-08 16:46:31 -0300

    project update

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata"; //importante vir primeiro
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
+import createConnection from "./database";
+import "./database/index.ts";
+import { router } from "./routes";
+import { AppError } from "./errors/AppError";
+
+
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, _next: NextFunction) => {
+        if(err instanceof AppError){
+            return response.status(err.statusCode).json({
+                message: err.message
+            });
+        }
+        return response.status(500).json({
+            status: "Errors",
+            message: `Internal server error ${err.message}`,        
+         })
+    }
+);
+
+export { app }
